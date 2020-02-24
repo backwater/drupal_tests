080b18483f10cb1801a5edcc8930131508805001
Dan Cauthorn <dcauthorn@theadkgroup.com>
2020-02-23 18:39:09 -0600

    Added arrays to template in module file and confirmed that I could access the array values in the template.

---
+++ html/modules/hello_world/fuzzy-page.tpl.php
@@ -0,0 +1,5 @@
+<section>
+
+	THIS IS FUZZY PAGE TPL !!
+
+</section>	
--- html/modules/hello_world/html-page.tpl.php
+++ html/modules/hello_world/html-page.tpl.php
@@ -5,3 +5,17 @@
    HEY - this is my hello world drupal module's html-page.tpl file !
 
 </div>
+
+<?php 
+
+      foreach( $fruits as $fr ){
+
+           echo $fr . '<br/>';
+      }
+
+      foreach( $veggies as $vg ){
+
+           echo $vg . '<br/>';
+      }
+
+?>
