710fd24b811900b5c4b49d1bb401155aa59bfbfb
Dan Cauthorn <dcauthorn@theadkgroup.com>
2020-03-03 15:10:27 -0600

    made some additional changes to get_country_region function

--- html/modules/licensed_vendors/licensed-vendors-page.tpl.php
+++ html/modules/licensed_vendors/licensed-vendors-page.tpl.php
@@ -9,11 +9,15 @@
 <?php 
    
      foreach( $vendors as $vendor ){
- 
+
           echo $vendor->title . '<br/>';
 
-          echo $vendor->field_vendor_address['und'][0]['value'] . '<br/>';
-        
+          echo $vendor->field_lv_city['und'][0]['value'] . '<br/>';
+
+          echo $vendor->region;
+
+          echo "<br/><hr/>";
+  
      }
 
 ?>
